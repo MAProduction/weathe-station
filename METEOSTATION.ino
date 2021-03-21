e2a5948750523523605ca02cc3370e6fb40af561
MAProduction <81111606+MAProduction@users.noreply.github.com>
2021-03-21 22:26:37 +0300

    Add files via upload

---
+++ METEOSTATION.ino
@@ -0,0 +1,82 @@
+/*
+Метеостанция на датчиках DHT
+Написал код M_A_Production
+Сайт maprod.ru
+*/
+//Библиотека датчика температуры
+#include "DHT.h"
+//Библиотеки дисплейного модуля
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+//Настройка датчика температуры
+#define DHTPIN 2 //Digital пин
+
+//Выберите свой датчик, надо раскоментировать подходящую модель
+#define DHTTYPE DHT11   // DHT 11
+//#define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
+//#define DHTTYPE DHT21   // DHT 21 (AM2301)
+DHT dht(DHTPIN, DHTTYPE);
+//Настройка дисплейного модуля по шине I2C
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+void setup() {
+
+  //Запуск дисплейного модуля
+  lcd.init();
+  lcd.init();
+  lcd.backlight();
+
+  //Запуск датчика температуры
+  dht.begin();
+}
+
+void loop() {
+  //Запись значений с датчика в переменные
+  float h = dht.readHumidity(); //Влажность
+  float t = dht.readTemperature(); //Темпратура
+  float f = dht.readTemperature(true);
+
+  //Вывод ошибки чтения данных с датчика
+  if (isnan(h) || isnan(t)) {
+    lcd.print(F("Failed to read from DHT sensor!"));
+    delay(100);
+    return;
+  }
+
+  //Подсчет температуры которую чувствует человек
+  float hif = dht.computeHeatIndex(f, h);
+  float hic = dht.computeHeatIndex(t, h, false);
+  //Вывод значений на экран
+
+  //Вывод влажности
+  lcd.setCursor(0, 0);
+  lcd.print("Hum:");
+  lcd.print(int(h));
+  lcd.setCursor(7, 0);
+  //Вывод температуры
+  lcd.print("Temp:");
+  if (t > 0 || t > 10) { //Логика вывода температуры
+    lcd.setCursor(13, 0);
+    lcd.print(int(t));
+    lcd.setCursor(14, 0);
+    lcd.print("C");
+  }
+  if (t < 10 || t < -1) {
+    lcd.setCursor(13, 0);
+    lcd.print(int(t));
+    lcd.setCursor(15, 0);
+    lcd.print("C");
+  }
+
+  //Вывод температуры которую чувствет человек
+  lcd.setCursor(0, 1);
+  lcd.print("Heat Index:");
+  lcd.setCursor(12, 1);
+  lcd.print(int(hic));
+  lcd.print(" C");
+
+  //обновление экрана
+  delay(2000);
+  lcd.clear();
+
+
+}
